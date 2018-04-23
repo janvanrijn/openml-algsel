c1e09828139fba75b6cfedd47c7680eb57570893
janvanrijn <janvanrijn@gmail.com>
2018-04-22 20:34:46 -0400

    added fanova on selector

--- examples/fanova_on_selector.py
+++ examples/fanova_on_selector.py
@@ -1,7 +1,13 @@
 import algsel
 import argparse
-import operator
+import ConfigSpace
+import fanova
+import numpy as np
+import os
+import pickle
 import sklearn
+
+from examples.run_on_oasc import run_on_scenario
 
 import sklearn.pipeline
 import sklearn.preprocessing
@@ -11,8 +17,111 @@ import sklearn.ensemble
 def parse_args():
     parser = argparse.ArgumentParser(description='Runs a sklearn algorithm on ASLib splits')
     parser.add_argument('--oasc_scenario_dir', type=str, default='../../oasc/oasc_scenarios/')
-    parser.add_argument('--scenario_name', type=str, default=None)
+    parser.add_argument('--cache_dir', type=str, default=os.path.expanduser("~") + '/experiments/fanova/cache')
+    parser.add_argument('--scenario_name', type=str, default='Oberon')
+    parser.add_argument('--lower_cutoff', type=float, default=None)
     parser.add_argument('--repeats', type=int, default=10)
+    parser.add_argument('--optimization_runs', type=int, default=150)
     parser.add_argument('--model', type=str, default='forest_256')
     return parser.parse_args()
 
+
+def get_config_space():
+    cs = ConfigSpace.configuration_space.ConfigurationSpace(name='Random Forest Algorithm Selection', seed=42)
+
+    imputation = ConfigSpace.hyperparameters.CategoricalHyperparameter('imputer__strategy', ['mean', 'median', 'most_frequent'], default_value="median")
+    # criterion = ConfigSpace.hyperparameters.CategoricalHyperparameter("classifier__criterion", ["gini", "entropy"], default_value="gini")
+    max_features = ConfigSpace.hyperparameters.UniformFloatHyperparameter("classifier__max_features", 0., 1., default_value=0.5)
+    n_estimators = ConfigSpace.hyperparameters.UniformIntegerHyperparameter("classifier__n_estimators", 8, 256, default_value=64, log=True)
+    min_samples_split = ConfigSpace.hyperparameters.UniformIntegerHyperparameter("classifier__min_samples_split", 2, 20, default_value=2)
+    min_samples_leaf = ConfigSpace.hyperparameters.UniformIntegerHyperparameter("classifier__min_samples_leaf", 1, 20, default_value=1)
+    bootstrap = ConfigSpace.hyperparameters.CategoricalHyperparameter("classifier__bootstrap", ["True", "False"], default_value="True")
+    single_model = ConfigSpace.hyperparameters.CategoricalHyperparameter("single_model", ["True", "False"], default_value="True")
+
+    cs.add_hyperparameters([imputation, n_estimators, max_features,
+                            min_samples_split, min_samples_leaf, bootstrap, single_model])
+    return cs
+
+
+def run_iteration(args, iteration_id, config):
+    cache_dir = os.path.join(args.cache_dir, args.scenario_name)
+    try:
+        os.makedirs(cache_dir)
+    except FileExistsError:
+        pass
+    cache_file = os.path.join(cache_dir, str(iteration_id) + '.pkl')
+    if os.path.isfile(cache_file):
+        with open(cache_file, 'rb') as fp:
+            print('Loaded iteration %d from cache' %iteration_id)
+            return pickle.load(fp)
+
+    pipeline = sklearn.pipeline.Pipeline(steps=[('imputer', sklearn.preprocessing.Imputer()),
+                                                ('classifier', sklearn.ensemble.RandomForestRegressor())])
+    sklearn_params = config.get_dictionary()
+    single_model = sklearn_params['single_model']
+    del sklearn_params['single_model']
+    pipeline.set_params(**sklearn_params)
+    meta = algsel.utils.ModelWrapper(pipeline, single_model)
+    result = run_on_scenario(args.oasc_scenario_dir, args.scenario_name, meta, args.repeats)
+
+    with open(cache_file, 'wb') as fp:
+        pickle.dump(result, fp)
+
+    return result
+
+
+def _do_fanova(config_space, configurations, performances, lower_cutoff):
+    X = list()
+
+    for configuration in configurations:
+        current = []
+
+        for param in config_space.get_hyperparameters():
+            value = configuration[param.name]
+
+            if isinstance(param, ConfigSpace.hyperparameters.CategoricalHyperparameter):
+                value = param.choices.index(value)
+
+            current.append(value)
+
+        X.append(current)
+
+    X = np.array(X)
+    y = np.array(performances)
+
+    cutoffs = (-np.inf, np.inf)
+    if lower_cutoff is not None:
+        p75 = np.percentile(y, lower_cutoff)
+        p100 = np.percentile(y, 100.0)
+        cutoffs = (p75, p100)
+
+    # start the evaluator
+    evaluator = fanova.fanova.fANOVA(X=X, Y=y, config_space=config_space, cutoffs=cutoffs, n_trees=128)
+
+    # obtain the results
+    result = {}
+
+    for idx, param in enumerate(config_space.get_hyperparameters()):
+        importance = evaluator.quantify_importance([idx])[(idx,)]['total importance']
+        result[param.name] = importance
+    return result
+
+
+def run(args):
+    config_space = get_config_space()
+    configurations = config_space.sample_configuration(args.optimization_runs)
+    performances = list()
+
+    for idx, configuration in enumerate(configurations):
+        result = run_iteration(args, idx, configuration)
+
+        # first save the results before doing fanova:
+        # paralleling and robustness
+        performances.append(result[1])
+
+    result = _do_fanova(config_space, configurations, performances, args.lower_cutoff)
+    print(result)
+
+
+if __name__ == '__main__':
+    run(parse_args())
--- examples/run_on_oasc.py
+++ examples/run_on_oasc.py
@@ -1,5 +1,6 @@
 import algsel
 import argparse
+import numpy as np
 import operator
 import sklearn
 
@@ -11,23 +12,16 @@ import sklearn.ensemble
 def parse_args():
     parser = argparse.ArgumentParser(description='Runs a sklearn algorithm on ASLib splits')
     parser.add_argument('--oasc_scenario_dir', type=str, default='../../oasc/oasc_scenarios/')
-    parser.add_argument('--scenario_name', type=str, default='Oberon')
-    parser.add_argument('--single_model', action='store_true', default=True)
+    parser.add_argument('--scenario_name', type=str, default='Camilla')
     parser.add_argument('--repeats', type=int, default=10)
     parser.add_argument('--impute', type=str, default='median')
     parser.add_argument('--model', type=str, default='forest_256')
     return parser.parse_args()
 
 
-def run(oasc_scenario_dir, scenario_name, impute, model, single_model, repeats):
+def run_on_scenario(oasc_scenario_dir, scenario_name, meta, repeats):
     train_frame, test_frame, description = algsel.utils.get_train_and_test_frame(oasc_scenario_dir, scenario_name)
     maximize = description['maximize'][0]
-
-    models = {
-        'tree': sklearn.tree.DecisionTreeRegressor(),
-        'forest_16': sklearn.ensemble.RandomForestRegressor(n_estimators=16),
-        'forest_256': sklearn.ensemble.RandomForestRegressor(n_estimators=256)
-    }
 
     test_tasks = set(test_frame['instance_id'].unique())
     avg_oracle_score = algsel.utils.oracle_score(test_frame, test_tasks, maximize)
@@ -35,18 +29,14 @@ def run(oasc_scenario_dir, scenario_name, impute, model, single_model, repeats):
     avg_best_score = algsel.utils.average_best_score(test_frame, avg_best_algorithm, test_tasks)
     golden_standard = algsel.utils.dataframe_to_scores(test_frame)
 
-    pipeline = sklearn.pipeline.Pipeline(steps=[('imputer', sklearn.preprocessing.Imputer(strategy=impute)),
-                                                ('classifier', models[model])])
-
+    task_scores = {task_id: list() for task_id in test_tasks}
     for seed in range(repeats):
         print(algsel.utils.get_current_time(), 'Training model on repeat', seed)
-        pipeline.set_params(classifier__random_state=seed)
-        meta = algsel.utils.ModelWrapper(pipeline, single_model)
+        meta.model_template.set_params(classifier__random_state=seed)
         meta.fit(train_frame)
 
         predictions = meta.predict(test_frame)
 
-        task_scores = {task_id: list() for task_id in test_tasks}
         for task_id, pred in predictions.items():
             if maximize:
                 predicted_algorithm = max(pred.items(), key=operator.itemgetter(1))[0]
@@ -54,6 +44,15 @@ def run(oasc_scenario_dir, scenario_name, impute, model, single_model, repeats):
                 predicted_algorithm = min(pred.items(), key=operator.itemgetter(1))[0]
 
             task_scores[task_id].append(golden_standard[task_id][predicted_algorithm])
+
+    if len(task_scores) != len(test_tasks):
+        raise ValueError()
+    for task_id, scores in task_scores.items():
+        if len(scores) != repeats:
+            raise ValueError('Expected %d scores, got %d' %(repeats, len(scores)))
+        if np.std(scores) == 0:
+            print('Taks %d all scores equal' %task_id)
+
 
     model_score, gap_score_single, gaps_stdev_single = algsel.utils.task_scores_to_avg(task_scores, avg_oracle_score, avg_best_score)
     return model_score, gap_score_single, gaps_stdev_single, avg_oracle_score, avg_best_score
@@ -67,10 +66,21 @@ if __name__ == '__main__':
     else:
         scenarios = ['Camilla', 'Oberon', 'Titus']
 
+    models = {
+        'tree': sklearn.tree.DecisionTreeRegressor(),
+        'forest_16': sklearn.ensemble.RandomForestRegressor(n_estimators=16),
+        'forest_256': sklearn.ensemble.RandomForestRegressor(n_estimators=256)
+    }
+
+    pipeline = sklearn.pipeline.Pipeline(steps=[('imputer', sklearn.preprocessing.Imputer(strategy=args.impute)),
+                                                ('classifier', models[args.model])])
+
     for scenario_name in scenarios:
-        print(algsel.utils.get_current_time(), args.model, 'on', scenario_name)
-        res = run(args.oasc_scenario_dir, scenario_name, args.impute, args.model, args.single_model, args.repeats)
-        model_score, gap_score_single, gaps_stdev_single, avg_oracle_score, avg_best_score = res
-        print(algsel.utils.get_current_time(), 'Oracle', avg_oracle_score)
-        print(algsel.utils.get_current_time(), 'Single Best', avg_best_score)
-        print(algsel.utils.get_current_time(), 'Score', model_score, 'GAP', gap_score_single, '+/-', gaps_stdev_single)
+        for single_model in [True, False]:
+            meta = algsel.utils.ModelWrapper(pipeline, single_model)
+            print(algsel.utils.get_current_time(), args.model, 'on', scenario_name, '; single model = ', single_model)
+            res = run_on_scenario(args.oasc_scenario_dir, scenario_name, meta, args.repeats)
+            model_score, gap_score_single, gaps_stdev_single, avg_oracle_score, avg_best_score = res
+            print(algsel.utils.get_current_time(), 'Oracle', avg_oracle_score)
+            print(algsel.utils.get_current_time(), 'Single Best', avg_best_score)
+            print(algsel.utils.get_current_time(), 'Score', model_score, 'GAP', gap_score_single, '+/-', gaps_stdev_single)
